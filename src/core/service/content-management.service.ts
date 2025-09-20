0f545902adc876b40f84f92541afb2b67b45acf3
keithi16 <keithishimogava16@gmail.com>
2025-09-20 19:27:13 -0300

    add entities

--- src/core/service/content-management.service.ts
+++ src/core/service/content-management.service.ts
@@ -1,5 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { VideoDAO } from '@src/persistence/dao/video.dao';
+import { Injectable } from '@nestjs/common';
+import { ContentEntity, ContentType } from '@src/core/entity/content.entity';
+import { MovieEntity } from '@src/core/entity/movie.entity';
+import { ThumbnailEntity } from '@src/core/entity/thumbnail.entity';
+import { VideoEntity } from '@src/core/entity/video.entity';
+import { ContentRepository } from '@src/persistence/repository/content.repository';
 
 export interface CreateContentData {
   title: string;
@@ -11,13 +15,26 @@ export interface CreateContentData {
 
 @Injectable()
 export class ContentManagementService {
-  constructor(
-    @Inject(VideoDAO)
-    private readonly videoDAO: VideoDAO,
-  ) {}
+  constructor(private readonly contentRepository: ContentRepository) {}
 
   async createContent(createContentData: CreateContentData) {
-    const createdVideo = this.videoDAO.create(createContentData);
-    return createdVideo;
+    const content = ContentEntity.createNew({
+      title: createContentData.title,
+      description: createContentData.description,
+      type: ContentType.MOVIE,
+      media: MovieEntity.createNew({
+        video: VideoEntity.createNew({
+          url: createContentData.url,
+          sizeInKb: createContentData.sizeInKb,
+          duration: 100,
+        }),
+        thumbnail: ThumbnailEntity.createNew({
+          url: createContentData.thumbnailUrl,
+        }),
+      }),
+    });
+
+    await this.contentRepository.create(content);
+    return content;
   }
 }
